269a6ba8051a11a98dc43036cd6be06f216bce4e
Abhishek Kanakagiri <57012567+kanakab@users.noreply.github.com>
2020-07-10 11:22:57 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+import re
+
+
+print('My calculator')
+#Ignore this, line added to learn git
+#Ignore this, line adde to try git diff
+print('Type "quit" to exit the program\n')
+
+run = True
+previous = 0
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input('Enter equation:')
+    else:
+        equation = input(str(equation))
+
+    if equation == 'quit':
+        print('Goodbye, Human!!!')
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z.,:()" "]', '', equation)
+        
+        if previous ==0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous)+ equation)
+    print(previous)
+
+while run:
+    performMath()
+
